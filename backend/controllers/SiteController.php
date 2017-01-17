4c42f19c374fd5964354a1399131a9241209c30f
phongvt3110 <vtphong651043@gmail.com>
2017-01-17 18:29:01 +0700

    update Sitecontroller and User model

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -5,7 +5,7 @@ use Yii;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
-use common\models\LoginForm;
+use common\models\db\LoginForm;
 
 /**
  * Site controller
